46098db9bfa58a6f872dc888a04f6e40704488db
Isaac Pante <isaac.pante@unil.ch>
2022-07-25 13:47:39 +0200

    Révision des dialogues & textes (anglais - Merci Cédric Margot !)

--- scripts/textes.js
+++ scripts/textes.js
@@ -1,5 +1,5 @@
 const textesIntroduction = [
-   "You just replied to an announce for a job in a handycraft company. Nothing to be happy with, but your loan is overdue and positions are almost inexistant in paleography.",
+   "You just replied to a job add from a handycraft company. Nothing to be happy with, but your loan is overdue and positions are almost inexistant in paleography.",
    'The appointment is set in a forest not far away from your neighborhood. Strange place for an interview, but "spicy encounters lead to spicy opportunities."',
    "At least, that's what Lydia said when she left you.",
 ];
@@ -18,50 +18,50 @@ const rencontreCastor = [
 ];
 
 const faits = [
-   // 1 predateurs : ours loups
-   "In Europe, beavers have had many wild predators for a very long time, including the wolf, the lynx or the brown bear. In America, the main predators of the beaver are the wolf, the coyote, the bear and the lynx. But its biggest predator remains man, who has hunted it intensively.",
+   // 1 prédateurs : ours loups
+   "In Europe, beavers have had many wild predators for a very long time, including wolves, lynx and brown bears. In America, the main predators of the beaver are wolves, coyotes, bears and lynx. But its biggest predator remain humans, who have hunted it intensively.",
    // 2 travail en meute
-   "The beaver is monogamous. It remains faithful to its partner throughout its life. Sexual maturity arrives around the age of three, at which time the young beavers are then driven away from their family unit.",
+   "The beaver is monogamous. It remains faithful to its partner throughout its life. Sexual maturity is reached around the age of three, when the young beavers are driven away from their family unit.",
    // 3 durée sous l'eau
-   "The beaver is capable of staying submerged for 4 to 5 minutes but its record is 15 minutes.",
+   "The beaver is capable of staying under water for 4 to 5 minutes but its record is 15 minutes.",
    // 4 mobilité des castors (5 ans)
    "If necessary, beavers may abandon their dam to create another den in a more favorable area.",
    // 5 comment vous considérez-vous ?
-   "beavers defend their territory vigorously and like to use their tails to make a loud noise to scare off predators.",
+   "beavers defend their territory vigorously and like to use their tail to make a loud noise to scare off predators.",
    // 6 deux ans de stage non payé
-   "The young stay with the family for two years, the time to learn the techniques of construction and defense of the territory and to take care of the youngest.",
+   "The young stay with the family for two years, the time to learn the techniques of construction (the art of dam building), territory defense and breeding.",
    // 7 activité du beaver
-   "A beaver is never satisfied with his home. He is constantly rearranging it, adding new branches and twigs, enlarging or modifying it. The English expression says it all: 'Work like beavers'.",
+   "A beaver is never satisfied with his home. He is constantly rearranging it, adding new branches and twigs, enlarging or modifying it. The English expression says it all: 'to work like a beaver'.",
    // 8 les barrages nécessaires
-   "Beaver dams can be up to two meters high. If they have an average length of 20 meters, the record is 850 meters.",
+   "Beaver dams can be up to two meters high. The average length is 20 meters; but the record is 850 meters.",
    // 9 le fonctionnement en famille
-   "The beaver is monogamous. It remains faithful to its partner throughout its life. Sexual maturity arrives around the age of three, at which time the young beavers are then driven away from their family unit.",
+   " The beaver is monogamous. It remains faithful to its partner throughout its life. Sexual maturity is reached around the age of three, when the young beavers are driven away from their family unit.",
    // 10 rouge ou bleu ?
    "Hunted for its oil, meat and fur, the beaver was driven to the brink of extinction in the early twentieth century. It has since been reintroduced in various territories, including Switzerland.",
    // 11 gérer les urgences
-   "The dams are built to regulate the water level so that the entrance to the burrow remains submerged and is protected from predators.",
+   "The dams are built to regulate the water level so that the entrance to the burrow remains submerged and protected from predators.",
    // 12 ours prédateurs
-   "Along with humans, bears are one of the most important predators of beavers.",
+   "Along with humans, bears are one of the main predators of beavers.",
    // 13 salaire argent trouvé
-   "Beavers are ally against drought : dams created by beavers raise the water table and cause water to spread",
+   "Beavers are the best allies against drought : dams created by beavers raise the water table and cause water to spread",
    // 14 ingénieur ou créatif
-   "The beaver is an extraordinary master builder. For its dams, it fixes vertically on the bottom of the stream branches and cut trunks, then stabilizes them with stones, mud, reeds and anything else that passes between its paws. With such constructions, beavers are more or less able to regulate a stream.",
+   "The beaver is an extraordinary master builder. For its dams, it fixes vertically on the bottom of the stream branches and cuts trunks, then stabilizes them with stones, mud, reeds and anything else that passes between its paws. With such constructions, beavers are more or less able to regulate a stream.",
    // 15 enfants & portées
-   "A pair of beavers gives birth to one or two young each year. After two years, they are then chased out of the den.",
+   "A pair of beavers gives birth to one or two pups each year. After two years, they are then chased out of the den.",
    // 16 rôle de la queue
-   "The beaver tail has many uses. It serves as a food reserve, helps it to move and propel itself under water, and also to stand upright. It also serves as a counterweight when carrying burdens, as a thermal regulator and as a sound deterrent when the beaver strikes it violently on the water.",
+   "The beaver’s tail has many uses. It serves as a food reserve, helps it to move and propel itself under water, and also to stand upright. It also serves as a counterweight when carrying burdens, as a thermal regulator and as a sound deterrent when the beaver strikes it violently on the water.",
    // 17 animal nocturne
    "The beaver is a nocturnal animal. At dusk, it leaves its den and goes in search of food in its territory. During the day, the whole family retires to the den to sleep.",
    // 18 véganisme
-   "The beaver is exclusively vegetarian. In summer, it adapts its food to what is available at the time. In winter, it becomes the specialist of willow bark.",
+   "The beaver is exclusively vegetarian. In summer, it adapts its meals to what is available. In winter, it becomes the specialist of willow bark.",
    // 19 caecotrophe
-   "The beaver has adapted to digest plant fibers and bark: the pouch in its cecum is huge and contains different strains of bacteria. In a first cycle, the food enters this diverticulum and is predigested by the bacteria. The beaver then excretes a white vegetable pulp rich in proteins and vitamins and re-ingests it. During the second pass through the digestive system, it can finally remove the vital elements.",
+   "The beaver has adapted to digest plant fibers and bark: the pouch in its cecum is huge and contains different strains of bacteria. In a first cycle, the food enters this diverticulum and is predigested by the bacteria. The beaver then excretes a white vegetable pulp rich in proteins and vitamins and re-ingests it. During the second passage through the digestive system, it can finally extract the vital elements.",
    // 20 délocalisation
-   "The beaver is monogamous. It remains faithful to its partner throughout its life. Sexual maturity arrives around the age of three, at which time the young beavers are then driven away from their family unit.",
+   " The beaver is monogamous. It remains faithful to its partner throughout its life. Sexual maturity is reached around the age of three, when the young beavers are driven away from their family unit.",
    // 21 taille du terrier
    "The entire beaver family inhabits a living area about one meter in diameter: the parents and two generations of young beavers.",
    // 22 hiérarchie
-   "The social system of beavers is unique among rodents. Each family occupies a discrete individual territory. Adults dominate the young and the young dominate the adults, each indicating their status through sounds, postures and gestures.",
+   "Beavers’ social system is unique among rodents. Each family occupies a discrete individual territory. Adults dominate the young and the young dominate the adults, each indicating their status through sounds, postures and gestures.",
    // 23 découverte de l'offre (quand visibles)
    // 24 unicité (big brain)
    "Their flat tail and webbed feet allow beavers to swim at speeds of up to 8 kilometres per hour.",
@@ -76,7 +76,7 @@ const questionInitiale = {
    sp1: 0,
    sc1: 0,
    rh2: "Am I crazy ?",
-   rc2: "Let me judge that. First question.",
+   rc2: "Let me be the judge of that. First question.",
    sp2: 0,
    sc2: 0,
 };
@@ -84,7 +84,7 @@ const questionInitiale = {
 /* rh1 : réponse humaine 1, rc : réponse castor, sp1 : score poste 1, sc1 : score confiance 1 */
 let interviews = [
    {
-      q: "Are you unconfortable with ethnic groups different than yours ?",
+      q: "Are you unconfortable with ethnic groups different from yours ?",
       rh1: "Only with bears and wolves.",
       rc1: "Bad answer, but I can relate.",
       sp1: -1,
@@ -108,7 +108,7 @@ let interviews = [
       idFact: 2,
    },
    {
-      q: "Can you work under water pressure ?",
+      q: "Can you work under water (pressure)?",
       rh1: "I'm a Pisces.",
       rc1: "Oh wow, lucky us.",
       sp1: -1,
@@ -120,12 +120,12 @@ let interviews = [
       idFact: 3,
    },
    {
-      q: "Where do you see you in five years ?",
-      rh1: "It depends on where you see YOU in five years.",
+      q: "Where do you see yourself in five years ?",
+      rh1: "It depends on where you see YOURSELF in five years.",
       rc1: "Nice one, but watch your back.",
       sp1: 1,
       sc1: 2,
-      rh2: "Where te company considers me the best.",
+      rh2: "Where the company considers me the best.",
       rc2: "Good boy.",
       sp2: 2,
       sc2: -1,
@@ -138,13 +138,13 @@ let interviews = [
       sp1: 2,
       sc1: -1,
       rh2: "The missing piece of your company.",
-      rc2: "Missing. Whas thinking about that.",
+      rc2: "Missing. I will think about that.",
       sp2: -1,
       sc2: -1,
       idFact: 5,
    },
    {
-      q: "There's a two year trial. Food apart, it's unpaid.",
+      q: "There's a two year trial. Apart from food, it's unpaid.",
       rh1: "I'll be happy to discover your cafeteria.",
       rc1: "The view is amazing.",
       sp1: 2,
@@ -156,7 +156,7 @@ let interviews = [
       idFact: 6,
    },
    {
-      q: "You'll have to be on the field. No part-time, no distance, full dedication.",
+      q: "You'll have to be on the field. No part-time, no distance (no remote working), full dedication.",
       rh1: "I have no life anyway.",
       rc1: "Perfect.",
       sp1: 2,
@@ -168,7 +168,7 @@ let interviews = [
       idFact: 7,
    },
    {
-      q: "Dam's are...",
+      q: "Dams are...",
       rh1: "A natural answer to climate crisis.",
       rc1: "Look who's talking.",
       sp1: 1,
@@ -206,7 +206,7 @@ let interviews = [
    {
       q: "Every emergency is an occasion to...",
       rh1: "Take a step back and think.",
-      rc1: "Say that when the dam is breaking.",
+      rc1: "Say that again when the dam is breaking.",
       sp1: -2,
       sc1: -1,
       rh2: "Dive into action.",
@@ -228,7 +228,7 @@ let interviews = [
       idFact: 12,
    },
    {
-      q: "The salary depends on money found in the river. Ok for you ?",
+      q: "The salary depends on the money found in the river. Ok for you ?",
       rh1: "I'm used to the volatility of the market.",
       rc1: "Remember your answer.",
       sp1: 2,
@@ -242,11 +242,11 @@ let interviews = [
    {
       q: "When you have to build something, you...",
       rh1: "Follow the instructions strictly.",
-      rc1: "Dam's are artistic mastercraft.",
+      rc1: "Dambuilding is artistic mastercraft.",
       sp1: -1,
       sc1: 0,
       rh2: "Allow room for creativity.",
-      rc2: "Dam's are precisely engineered.",
+      rc2: "Dams are precisely engineered.",
       sp2: -1,
       sc2: 0,
       idFact: 14,
@@ -258,7 +258,7 @@ let interviews = [
       sp1: -1,
       sc1: 1,
       rh2: "I plan to find a girlfriend.",
-      rc2: "Stay away of my daughter.",
+      rc2: "Stay away from my daughter.",
       sp2: 1,
       sc2: -1,
       idFact: 15,
@@ -289,7 +289,7 @@ let interviews = [
    },
    {
       q: "Veganism is...",
-      rh1: "The cannibalism of the weaks.",
+      rh1: "The cannibalism of the weak.",
       rc1: "Ok, THAT is insulting.",
       sp1: -3,
       sc1: 0,
@@ -317,7 +317,7 @@ let interviews = [
       rc1: "Nah.",
       sp1: -1,
       sc1: 1,
-      rh2: "Just take me away.",
+      rh2: "Just take me with you.",
       rc2: "Cool down human.",
       sp2: 2,
       sc2: -1,
@@ -337,7 +337,7 @@ let interviews = [
    },
    {
       q: "Your manager is your manager because...",
-      rh1: "Ask the manager, he only knows.",
+      rh1: "Ask the manager, only he knows.",
       rc1: "So true.",
       sp1: 1,
       sc1: -1,
@@ -372,7 +372,7 @@ let interviews = [
       idFact: 24,
    },
    {
-      q: "We're an architect's company. Should we follow the land planning legislation ?",
+      q: "We're an architecture company. Should we follow the land planning legislation ?",
       rh1: "You rule.",
       rc1: "Yup.",
       sp1: 1,
@@ -424,7 +424,7 @@ const ppen = [
    "Beaver's Comp. rejected you, just as Lydia did.",
    "You know what ?",
    "Good news.",
-   "Maybe it's time for you to stop knocking at every door, looking for approval.",
+   "Maybe it's time for you to stop knocking on every door, looking for approval.",
    "You'll get back to your flat, pack some stuff and go on a trip.",
    "You'll spice up your life by yourself.",
    "You're not a looser.",
@@ -437,15 +437,15 @@ const pnep = [
    "It's been three month now. The company is really happy with your results.",
    "Spicy times, for sure.",
    "As a matter of fact, they found any single way to overuse you.",
-   "A night with three hours of sleep is a feast, food is orrendous and you're now sure of one thing.",
+   "A night with three hours of sleep is a feast, food is horrendous and you're now sure of one thing.",
    "You'll never be one of them.",
-   "But deep in yourself, you know that's justice.",
-   "Your species was so hard with them.",
+   "But deep inside, you know that's justice.",
+   "Your species was so hard on them.",
    "Better stay here. Nowadays, all the companies are equal.",
-   "How did Thomas Bernhard said already ?",
+   "What did Thomas Bernhard say again ?",
    "Oh yes.",
-   "It's equal.",
-   "Everything's equal.",
+   "It doesn’t matter.",
+   "It all doesn’t matter.",
 ];
 
 // perso négatif, entreprise négative : fond sombre
@@ -453,13 +453,13 @@ const pnen = [
    "You tried to spice up your life but it's crystal clear now : there's no way out.",
    "There never was.",
    "Everybody rejects you.",
-   "Lydia one month ago, Beaver company now.",
-   "For strange reasons, you're condemned to do the worse choices any-single-time.",
+   "Lydia last month, Beaver company now.",
+   "For strange reasons, you're doomed to make the worst choice every-single-time.",
    "What will you do now ?",
    "Go home ?",
    "Your flat is no home to you.",
    "It's a mess, a threat.",
-   "You look at the stars, sky is cloudy.",
+   "You look at the stars, the sky is cloudy.",
    "You only see dark branches.",
    "You touch your tie.",
    "Here's your way out.",
